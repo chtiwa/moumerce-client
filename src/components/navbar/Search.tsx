c06113c8a99442d0f290418df1b83cc0e1a3a76e
chtiwa <chtiwaa@gmail.com>
2023-08-09 17:34:35 +0100

    done

--- src/components/navbar/Search.tsx
+++ src/components/navbar/Search.tsx
@@ -1,66 +1,82 @@
-import { useState, useEffect, useRef } from 'react'
-import './Search.scss'
-import { AiOutlineClose, AiOutlineSearch } from 'react-icons/ai'
-import { useAppDispatch, useAppSelector } from '../../features/hooks'
-import { closeSearch } from '../../features/searchSlice'
-import { useSearchProductsQuery } from '../../services/products'
-import { Link } from 'react-router-dom'
+import { useState, useEffect, useRef } from "react";
+import "./Search.scss";
+import { AiOutlineClose, AiOutlineSearch } from "react-icons/ai";
+import { useAppDispatch, useAppSelector } from "../../features/hooks";
+import { closeSearch } from "../../features/searchSlice";
+import { useSearchProductsQuery } from "../../services/products";
+import { Link } from "react-router-dom";
 
 const Search = () => {
-  const dispatch = useAppDispatch()
-  const { isSearchOpen } = useAppSelector(state => state.search)
-  const [search, setSearch] = useState('')
-  const componentRef = useRef(null)
-  const { data, isLoading, isError } = useSearchProductsQuery(search)
+  const dispatch = useAppDispatch();
+  const { isSearchOpen } = useAppSelector((state) => state.search);
+  const [search, setSearch] = useState("");
+  const componentRef = useRef(null);
+  const { data, isLoading, isError } = useSearchProductsQuery(search);
 
   const handleSubmit = (e: any) => {
-    e.preventDefault()
-  }
+    e.preventDefault();
+  };
 
   const handleChange = (e: any) => {
-    setSearch(e.target.value)
-  }
+    setSearch(e.target.value);
+  };
 
   useEffect(() => {
     const handleEscapeKey = (e: any) => {
-      if (e.key === 'Escape') {
-        dispatch(closeSearch())
+      if (e.key === "Escape") {
+        dispatch(closeSearch());
       }
-    }
+    };
 
-    document.addEventListener('keydown', handleEscapeKey)
+    document.addEventListener("keydown", handleEscapeKey);
 
     return () => {
-      document.removeEventListener('keydown', handleEscapeKey)
-    }
-  }, [])
+      document.removeEventListener("keydown", handleEscapeKey);
+    };
+  }, []);
 
   return (
-    <div className={`${isSearchOpen ? 'show-search' : 'hide-search'}`}>
-      <div className='close-icon' onClick={() => {
-        dispatch(closeSearch())
-      }}>
+    <div className={`${isSearchOpen ? "show-search" : "hide-search"}`}>
+      <div
+        className="close-icon"
+        onClick={() => {
+          dispatch(closeSearch());
+        }}
+      >
         <AiOutlineClose />
       </div>
-      <form onSubmit={handleSubmit} ref={componentRef} >
-        <div className="form-control" >
-          <input type="text" name='search' value={search || ''} onChange={handleChange} placeholder='search for products' />
+      <form onSubmit={handleSubmit} ref={componentRef}>
+        <div className="form-control">
+          <input
+            type="text"
+            name="search"
+            value={search || ""}
+            onChange={handleChange}
+            placeholder="search for products"
+          />
           <button>
             <AiOutlineSearch />
           </button>
           <ul className="list">
-            {search.length > 0 && !isLoading && !isError && data.products.length > 0 && (
+            {search.length > 0 &&
+              !isLoading &&
+              !isError &&
+              data.products.length > 0 &&
+              // @ts-ignore
               data.products.map((product, index) => (
-                <Link to={`/product/${product._id}`} key={index} onClick={() => dispatch(closeSearch())} >
+                <Link
+                  to={`/product/${product._id}`}
+                  key={index}
+                  onClick={() => dispatch(closeSearch())}
+                >
                   <li className="item">{product.title} </li>
                 </Link>
-              ))
-            )}
+              ))}
           </ul>
         </div>
       </form>
     </div>
-  )
-}
+  );
+};
 
-export default Search
+export default Search;
